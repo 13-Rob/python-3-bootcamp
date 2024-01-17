aeb4d83bb39949757a7613622f5ff5507d7a325b
Roberto Gonzalez <rcgr.13@gmail.com>
2024-01-16 16:33:36 -0800

    Changes to comply with pylint

--- My-Files/08-Milestone_Project_2/Blackjack/main.py
+++ My-Files/08-Milestone_Project_2/Blackjack/main.py
@@ -33,52 +33,58 @@ def main():
     dealer = Dealer("Dealer", 1000000)
 
     op = 'y'
-    # op = input('Dealer: Do you want to play Blackjack? Y/n\n').lower()
+    # Game logic goes here
+    # name = input("Dealer: What should we call you?\n")
+    player = Player("Rob")
 
-    if op.startswith('y'):
-        game_on = True
-        # Game logic goes here
-        # name = input("Dealer: What should we call you?\n")
-        player = Player("Rob")
+    while op.startswith('y'):
+        system('cls')
+        deck = Deck()
+        deck.shuffle()
+        player.empty_hand()
+        dealer.empty_hand()
 
-        while game_on:
-            system('cls')
-            deck = Deck()
-            deck.shuffle()
-            player.empty_hand()
-            dealer.empty_hand()
+        print("Dealer: Your balance is "
+            + Fore.GREEN + f"${player.balance}.00"
+            + Style.RESET_ALL + ".")
 
-            print("Dealer: Your balance is "
-                + Fore.GREEN + f"${player.balance}.00"
-                + Style.RESET_ALL + ".")
+        # Bets here: Default 10
+        if player.balance > 0:
+            total_bet = player.withdraw() + dealer.withdraw()
 
-            # Bets here: Default 10
-            if player.balance > 0:
-                total_bet = player.withdraw() + dealer.withdraw()
+        print("The total bet is "
+            + Fore.GREEN + f"${total_bet}.00"
+            + Style.RESET_ALL + ".")
 
-            print("The total bet is "
-                + Fore.GREEN + f"${total_bet}.00"
-                + Style.RESET_ALL + ".")
+        player.add_card(deck.deal_one())
+        dealer.add_card(deck.deal_one())
+        player.add_card(deck.deal_one())
+        dealer.add_card(deck.deal_one())
 
+        player.show_hand()
+        dealer.show_hand()
+
+        # Ask the player if they want to hit or stay
+        op = input("Dealer: Hit or Stay? H/s\n").lower()
+
+        while op.startswith('h'):
             player.add_card(deck.deal_one())
-            dealer.add_card(deck.deal_one())
-            player.add_card(deck.deal_one())
-            dealer.add_card(deck.deal_one())
-
             player.show_hand()
+            op = input("Delaer: Hit or Stay? H/s\n").lower()
+
+        player.show_hand()
+
+        # Dealer decides if they want to hit or stay
+        if (dealer.hand_value < 21 and
+                player.hand_value > dealer.hand_value and
+                player.hand_value <= 21):
+            op = 'h'
+        else:
+            op = 's'
+
+        while op.startswith('h'):
+            dealer.add_card(deck.deal_one())
             dealer.show_hand()
-
-            # Ask the player if they want to hit or stay
-            op = input("Dealer: Hit or Stay? H/s\n").lower()
-
-            while op.startswith('h'):
-                player.add_card(deck.deal_one())
-                player.show_hand()
-                op = input("Delaer: Hit or Stay? H/s\n").lower()
-
-            player.show_hand()
-
-            # Dealer decides if they want to hit or stay
             if (dealer.hand_value < 21 and
                     player.hand_value > dealer.hand_value and
                     player.hand_value <= 21):
@@ -86,39 +92,26 @@ def main():
             else:
                 op = 's'
 
-            while op.startswith('h'):
-                dealer.add_card(deck.deal_one())
-                dealer.show_hand()
-                if (dealer.hand_value < 21 and
-                        player.hand_value > dealer.hand_value and
-                        player.hand_value <= 21):
-                    op = 'h'
-                else:
-                    op = 's'
+        # Resolve game
+        player.show_hand()
+        dealer.show_full_hand()
 
-            # Resolve game
-            player.show_hand()
-            dealer.show_full_hand()
+        winner = det_winner(player, dealer)
 
-            winner = det_winner(player, dealer)
+        if winner == 1:
+            print("Dealer: *sigh* You won")
+            player.deposit(total_bet)
+        elif winner == -1:
+            print("Dealer: I won")
+            dealer.deposit(total_bet)
+        else:
+            print("Dealer: It's a tie...")
+            player.deposit(total_bet/2)
+            dealer.deposit(total_bet/2)
 
-            if winner == 1:
-                print("Dealer: *sigh* You won")
-                player.deposit(total_bet)
-            elif winner == -1:
-                print("Dealer: I won")
-                dealer.deposit(total_bet)
-            else:
-                print("Dealer: It's a tie...")
-                player.deposit(total_bet/2)
-                dealer.deposit(total_bet/2)
+        total_bet = 0
 
-            total_bet = 0
-
-            op = input("Do you want to play again? (Y/n)\n").lower()
-
-            if not op.startswith('y'):
-                game_on = False
+        op = input("Do you want to play again? (Y/n)\n").lower()
 
         # Repeat
 
